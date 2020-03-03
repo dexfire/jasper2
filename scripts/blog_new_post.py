700f516af9f07636bba624ddcef817c29168a118
dexfire <dexfire29@foxmail.com>
2020-03-03 15:04:12 +0800

    added some posts

--- scripts/blog_new_post.py
+++ scripts/blog_new_post.py
@@ -1,5 +1,13 @@
 import argparse
+import datetime
+
+"""
+用于产生一个新的 Post 文件，自动分类
+"""
 
 ap = argparse.ArgumentParser(
     "blog_new_post", epilog="a script to create new Jekyll Post.")
-ap.add_argument("--title", dest="title", action="")
+ap.add_argument("--title", "-t", dest="title", action="store", type=str)
+ap.add_argument("--tags", dest="tags", nargs="*", action="store")
+
+print(datetime.datetime())
